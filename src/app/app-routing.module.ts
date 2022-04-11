52cd13395206027155968818146242633fcf4b57
Carlos-Melo <carloszdu@gmail.com>
2022-04-11 18:03:27 -0300

    Login e Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoletosComponent } from './boletos/boletos.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'boletos', component: BoletosComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  //{ path: '**', component:  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuHomeComponent } from './menu-home/menu-home.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { BoletosComponent } from './boletos/boletos.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuHomeComponent,
+    MenuComponent,
+    LoginComponent,
+    CadastroComponent,
+    PerfilComponent,
+    BoletosComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
